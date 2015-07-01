773e916ba511f35dbeaab4f83d9c9b6fc8a161bd
Vladk-el <eliott.laversin@gmail.com>
2015-07-02 00:34:04 +0200

    dns client ok

--- DNSClient/src/main/java/com/vadkel/full/dns/server/dns/client/Main.java
+++ DNSClient/src/main/java/com/vadkel/full/dns/server/dns/client/Main.java
@@ -1,5 +1,12 @@
 package com.vadkel.full.dns.server.dns.client;
 
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+import com.vadkel.full.dns.server.common.utils.dns.DNS;
+import com.vadkel.full.dns.server.dns.client.model.DNSQuery;
+
 /**
  * Hello world!
  *
@@ -7,7 +14,26 @@ package com.vadkel.full.dns.server.dns.client;
 public class Main {
 	
     public static void main( String[] args ) {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello DSNClient!" );
+        
+        try {
+			// QueryDNS ==> 
+			DNSQuery query = new DNSQuery("google.fr", DNS.QTYPE_ANY, DNS.CLASS_IN);
+			
+			
+			DatagramSocket socket = new DatagramSocket();			
+			socket.setSoTimeout(5000);
+
+			byte[] data = query.buildQuery();
+
+			System.out.println("Length : " + data.length);
+			DatagramPacket packet = new DatagramPacket(data, data.length, InetAddress.getByName("8.8.8.8"), 53);
+			socket.send(packet);
+
+			System.out.println("END");
+		} catch (Exception E) {
+
+		}
     }
     
 }
--- DNSClient/src/main/java/com/vadkel/full/dns/server/dns/client/model/DNSQuery.java
+++ DNSClient/src/main/java/com/vadkel/full/dns/server/dns/client/model/DNSQuery.java
@@ -1,5 +1,12 @@
 package com.vadkel.full.dns.server.dns.client.model;
 
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+import com.vadkel.full.dns.server.common.utils.dns.DNS;
+
 public class DNSQuery {
 
 	private static int id;
@@ -18,7 +25,70 @@ public class DNSQuery {
 	}
 	
 	public DNSQuery(String host, int type, int clazz) {
+		StringTokenizer labels = new StringTokenizer(host, ".");
 		
+		while (labels.hasMoreTokens())
+			if (labels.nextToken().length() > DNS.LIMIT_SIZE_LABEL)
+				throw new IllegalArgumentException("Invalid hostname: " + host);
+		queryHost = host;
+		queryType = type;
+		queryClass = clazz;
+		
+		synchronized (getClass()) {
+			queryID = (++id) % 65536;
+		}
+	}
+	
+	public byte[] buildQuery() {
+		ByteArrayOutputStream byteArrayOut = new ByteArrayOutputStream();
+		DataOutputStream dataOut = new DataOutputStream(byteArrayOut);
+		try {
+			
+			// ID
+			dataOut.writeShort(queryID);
+			
+			System.out.println("(0 << DNS.SHIFT_QUERY)" + DNS.SHIFT_QUERY + " : " + (0 << DNS.SHIFT_QUERY));
+			
+			System.out.println("(DNS.OPCODE_QUERY << DNS.SHIFT_OPCODE)" + DNS.OPCODE_QUERY + " ET " + DNS.SHIFT_OPCODE + " : " + (DNS.OPCODE_QUERY << DNS.SHIFT_OPCODE));
+			
+			System.out.println("(1 << DNS.SHIFT_RECURSE_PLEASE)" + DNS.SHIFT_RECURSE_PLEASE + " : " + (1 << DNS.SHIFT_RECURSE_PLEASE));
+			
+			
+			// Flags
+			dataOut.writeShort((0 << DNS.SHIFT_QUERY) // 0 décallé de 15 car REQUETE ==> [0]000 0000 0000 0000
+					| (DNS.OPCODE_QUERY << DNS.SHIFT_OPCODE) // 0 décallé de 11 == > 0000 [0]000 0000 0000
+					| (1 << DNS.SHIFT_RECURSE_PLEASE)); // 1 décallé de 8 ==> 0000 000[1] 0000 0000
+			
+			/**
+			 * ==> [0]000 0000 0000 0000 | 0000 [0]000 0000 0000 | 0000 000[1] 0000 0000 ==> 0000 0001 0000 0000 (256)
+			 */
+			
+			// Questions number
+			dataOut.writeShort(1);
+			
+			// Responses number
+			dataOut.writeShort(0);
+			
+			// Authority RR number
+			dataOut.writeShort(0);
+			
+			// Additionnal RR number
+			dataOut.writeShort(0);
+
+			StringTokenizer labels = new StringTokenizer(queryHost, ".");
+			while (labels.hasMoreTokens()) {
+				String label = labels.nextToken();
+				System.out.println("Label : " + label);
+				dataOut.writeByte(label.length());
+				dataOut.writeBytes(label);
+			}
+			
+			dataOut.writeByte(0);
+			dataOut.writeShort(queryType);
+			dataOut.writeShort(queryClass);
+		} catch (IOException ignored) {
+		}
+		return byteArrayOut.toByteArray();
 	}
 
 	public static int getId() {
